f4e7e457b37bd04ebf5bc2775ddc32120e1778b7
tomtiger91 <57660656+tomtiger91@users.noreply.github.com>
2020-01-16 09:28:38 +0100

    Add files via upload

---
+++ map_survie.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const map_survie = function (bfld, query) {
+
+    let html;
+    let i;
+    let j;
+
+    html = '';
+
+    for (i = 0; i < bfld.length; i++) {
+        html += '<div class="ligne">';
+        for (j = 0; j < bfld[i].length; j++) {
+            if (bfld[i][j] === " " || bfld[i][j] === "1"){
+                html += '<div class="land"></div>';
+            }else if(bfld[i][j] === "x"){
+                if(query.action === "Haut"){
+                    //if(bfld[i-1][j] === " "){
+                        html += '<div class="land"><div class="perso up"></div></div>';
+                    /*} else {
+                        html += '<div class="land"><div class="perso up"></div></div>';
+                    }*/
+                } else if(query.action === "Bas"){
+                    //if(bfld[i+1][j] === " "){
+                        html += '<div class="land"><div class="perso down"></div></div>';
+                    /*} else {
+                        html += '<div class="land"><div class="down"></div></div>';
+                    }*/
+                } else if(query.action === "Gauche"){
+                    //if(bfld[i][j-1] === " "){
+                        html += '<div class="land"><div class="perso left"></div></div>';
+                    /*} else { 
+                        html += '<div class="land"><div class="left"></div></div>';
+                    }*/
+               
+			   } else if(query.action === "Droite"){
+                    //if(bfld[i][j+1] === " "){
+                        html += '<div class="land"><div class="perso right"></div></div>';
+                    /*}else { 
+                        html += '<div class="land"><div class="right"></div></div>';
+                    }*/
+
+                } else if(query.action === "Attaquer"){
+                    html += '<div class="land"><img class="perso" src="Knif-right.gif" /></div>';
+
+                } else {
+                    html += '<div class="land"><div class="perso right"></div></div>';
+
+                }
+            }else if(bfld[i][j] === "o"){
+                html += '<div class="land"><img class="ghost" src="monster.gif" /></div>';
+            }
+
+        }
+        html += '</div>';
+    }
+
+    return html;
+};
+
+
+module.exports = map_survie;
